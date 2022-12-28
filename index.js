ed0c325fc11201275e1b801d5d24e7d584de2ae0
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2022-12-28 20:30:01 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rx4i6uo.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error);
 app.get('/', (req, res) => {
     res.send('social media');
 })
